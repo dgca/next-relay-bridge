27df3e0f0b5a582754ad63111e0b8be9c482d140
Dan Cortes <hi@dancortes.dev>
2021-08-27 00:33:14 -0600

    Remove unused import

--- example/pages/users/index.tsx
+++ example/pages/users/index.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React from "react";
 import { usePreloadedQuery } from "react-relay";
 import { graphql } from "relay-runtime";
 import Link from "next/link";
