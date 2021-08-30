1fcc4fe07cd71980ca67bbde4be88402d8306efe
Bishop Zareh <xchicago@gmail.com>
2021-08-30 15:56:00 -0600

    add type specificity

--- example/pages/users/index.tsx
+++ example/pages/users/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { usePreloadedQuery } from "react-relay";
-import { graphql } from "relay-runtime";
+import { usePreloadedQuery, PreloadedQuery } from "react-relay";
+import { graphql, GraphQLTaggedNode } from "relay-runtime";
 import Link from "next/link";
 
 import { withPageBridge, SafeSuspense } from "relay/bridge";
 
 import { users_pageQuery as users_pageQueryType } from "queries/__generated__/users_pageQuery.graphql";
+import { NextPageContext } from "next";
 
 const users_pageQuery = graphql`
   query users_pageQuery {
@@ -25,7 +26,12 @@ const users_pageQuery = graphql`
   }
 `;
 
-function Users({ usersQuery }) {
+interface UsersProps {
+  usersQuery: PreloadedQuery<users_pageQueryType, Record<string, unknown>>
+}
+
+function Users({ usersQuery }: UsersProps) {
+  console.log(usersQuery)
   const data = usePreloadedQuery<users_pageQueryType>(
     users_pageQuery,
     usersQuery
@@ -41,7 +47,7 @@ function Users({ usersQuery }) {
   );
 }
 
-function UsersWrapper(props: any) {
+function UsersWrapper(props: UsersProps) {
   return (
     <SafeSuspense fallback="Loading...">
       <Users {...props} />
@@ -49,17 +55,24 @@ function UsersWrapper(props: any) {
   );
 }
 
+interface BridgeWrapperProps {
+  context: NextPageContext,
+  preloadQuery: (
+    users_pageQuery: GraphQLTaggedNode,
+    options: Record<string, unknown>
+  ) => unknown
+}
+
 export default withPageBridge({
   PageComponent: UsersWrapper,
-  getInitialProps: async ({ context, preloadQuery }) => {
-    /**
-     * If we needed to redirect from this page, we can return an object
-     * like the one below. If the returned props have a `redirect` key of type
-     * `{ destination: string, permanent?: boolean | number }`, we'll redirect the user
-     * to the destination. `permanent` defaults to `false`. If permanent is `false`,
-     * we'll treat it as a 307 redirect. If it's true, it'll be a 308. You can also pass
-     * `permanent` a redirect status code to set it directly.
-     */
+  getInitialProps: async ({ context, preloadQuery }: BridgeWrapperProps ) => {
+
+    // If we needed to redirect from this page, we can return an object
+    // like the one below. If the returned props have a `redirect` key of type
+    // `{ destination: string, permanent?: boolean | number }`, we'll redirect the user
+    // to the destination. `permanent` defaults to `false`. If permanent is `false`,
+    // we'll treat it as a 307 redirect. If it's true, it'll be a 308. You can also pass
+    // `permanent` a redirect status code to set it directly.
     // return {
     //   redirect: {
     //   destination: "/",
